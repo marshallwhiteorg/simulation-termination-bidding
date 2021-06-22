e7e70af2f831afe0495d4aa68cb9d21958c33383
Marshall White <mw@marshallwhite.org>
2021-06-22 14:42:15 -0600

    Remove unused variable name

--- gen.py
+++ gen.py
@@ -2,7 +2,7 @@ import uuid
 import random
 
 
-for idx in range(1000):
+for _ in range(1000):
     uid = uuid.uuid4()
     eudaimonia = random.randint(0, 999999999)
     print('<tr>\n<td>{}</td>\n<td>{}</td>\n<td><button class="button">!PLACE BID!</button></td>\n</tr>'.format(uid, eudaimonia))
